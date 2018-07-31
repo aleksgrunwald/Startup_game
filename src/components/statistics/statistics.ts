4fa8a0573e92daf21f26fb4160d91837579b5986
Aleks <aleks.grunwald@gmail.com>
2018-07-31 20:29:00 +0200

    Deleted OnInit from StatisticsComponent

--- src/components/statistics/statistics.ts
+++ src/components/statistics/statistics.ts
@@ -1,32 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { StatisticsServiceProvider } from '../../providers/statistics-service/statistics-service';
 
-/**
- * Generated class for the StatisticsComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
+
 @Component({
   selector: 'statistics',
   templateUrl: 'statistics.html'
 })
 
-export class StatisticsComponent implements OnInit{
+export class StatisticsComponent {
 
   statistics: any = [];
 
   constructor(public statisticsProvider: StatisticsServiceProvider) {
   }
 
-  ngOnInit() {
-  }
-
   
   getStatistics() {
     this.statisticsProvider.downloadStatistics().subscribe(data => {
       this.statistics = data;
-      console.log('new numbers')
+      console.log(data)
     })
   }
 
