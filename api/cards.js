e401507a0e86d03052a220010f9efa0743ae7e1e
Anton Katyrenchuk <s17505@pjwstk.edu.pl>
2018-08-02 00:40:02 +0200

    Logical cards added

--- api/cards.js
+++ api/cards.js
@@ -1,17 +1,20 @@
 module.exports = [
   {
-    title: "Karta 1",
-    cashChange: 3,
-    question: "Tak czy nie?"
+    title: "New employee!",
+    description: "A new programmer has come to us, he has 10 years experience at Microsoft.",
+    question: "Do you want to hire him?",
+    pictureURL: "https://us.123rf.com/450wm/pppfoto15/pppfoto151501/pppfoto15150100077/35260131-stock-vector-new-employee-in-the-office-for-new-job.jpg?ver=6"
   },
   {
-    title: "Karta 2",
-    cashChange: 50,
-    question: "Nie czy tak?"
+    title: "Env-friendly organization offers You a contract.",
+    description: "Hi, *firma name*, do you want to take part in our new project? \nIt`s about saving Amazonian forests..\n We collect 100 000$ from each firm-partner. \nAs a reward you will get free advertisement!",
+    question: "Would you like to risk? Our analytics are not sure about this 'advertisement'.. ",
+    pictureURL: "http://storylawfirm.com/wp-content/uploads/2014/01/Arkansas-Contract-Law-Story-Law-Firm-PLLC-400x400.jpg"
   },
   {
-    title: "Karta 3",
-    cashChange: -19,
-    question: "Chcesz tego pracownika ?"
+    title: "New computers.",
+    description: "Consulting department is asking for new PC, their work is done slowlier because of the RAM deficiency",
+    question: "Do they really need the PC upgradement?",
+    pictureURL: "http://www.pcsteven.com/images/caveman-upgrade.jpg"
   }
 ];
--- api/model.js
+++ api/model.js
@@ -1,8 +1,11 @@
 const tick = (params, decision, card) => {
   if (decision) {
-    params.param1 = Math.floor(Math.random() * 1000);
-    params.cash += card.cashChange;
+    params.param1 += Math.floor((Math.random() - 0.5) * 200);
+    params.param2 += Math.floor((Math.random() - 0.5) * 200);
+    params.cash += Math.floor((Math.random() - 0.5) * 200);
   } else {
+    params.param1 += Math.floor((Math.random() - 0.5) * 200);
+    params.param2 += Math.floor((Math.random() - 0.5) * 200);
     params.cash += Math.floor((Math.random() - 0.5) * 200);
   }
   return params;
