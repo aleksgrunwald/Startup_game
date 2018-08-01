69e886896f44b0556857f67e490026e2b65b9d8e
dendrebeden <shushpanovdenis@gmail.com>
2018-08-01 22:13:40 +0200

    statistics comparison added

--- src/components/statistics/statistics.ts
+++ src/components/statistics/statistics.ts
@@ -10,15 +10,37 @@ import { StatisticsServiceProvider } from '../../providers/statistics-service/st
 export class StatisticsComponent {
 
   statistics: any = [];
+  changedStatistics: any = {};
 
   constructor(public statisticsProvider: StatisticsServiceProvider) {
   }
 
   
   getStatistics() {
+
     this.statisticsProvider.downloadStatistics().subscribe(data => {
+
+      for (var i = 0; i < this.statistics.length; i++) {
+        this.changedStatistics[i] = {};
+
+        if (this.statistics.data[i] == data.data[i])  {
+        
+          this.changedStatistics[i].changed = false;
+        
+        } else {
+
+          this.changedStatistics[i].changed = true
+
+          if (this.statistics.data[i] > data.data[i]) this.changedStatistics[i].color = true;
+
+          else this.changedStatistics[i].color = false;
+
+        }
+      }
+
+      console.log(this.changedStatistics);
+
       this.statistics = data;
-      console.log(data)
     })
   }
 
